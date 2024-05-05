8a4b6cc794ef2a32fe579f988b0ccdb8192d6834
TahaJuzar <tahajuzar5tj@gmail.com>
2024-05-05 16:56:38 +0500

    adding file

---
+++ addition.py
@@ -0,0 +1,8 @@
+num = input("write a number :")
+print(num)
+num2 = input("write a second number:")
+print(num2)
+sum = int(num) + int(num2)
+print(sum)
+
+
---
+++ functions.py
@@ -0,0 +1,10 @@
+course = ("I'am Taha Juzar")
+print(course.upper())
+print(course.find('J'))
+
+
+print(course.replace("am", 'om'))
+
+print( 'python' in course)
+
+
